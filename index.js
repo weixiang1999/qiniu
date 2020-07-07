64282e61b1ce70333c5c4d096aa7913f3398b81b
weixiang <920666487@qq.com>
2020-07-07 16:21:09 +0800

    fix: 修复报错问题

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
+import { NativeModules, NativeEventEmitter } from "react-native";
 
-import { NativeModules, NativeEventEmitter } from 'react-native'
+const { RNTQiniu } = NativeModules;
 
-const { RNTQiniu } = NativeModules
+const eventEmitter = new NativeEventEmitter(RNTQiniu);
 
-const eventEmitter = new NativeEventEmitter(RNTQiniu)
+const index2callback = {};
 
-const index2callback = {}
+let count = 0;
 
-let count = 0
-
-eventEmitter.addListener('progress', function (data) {
-  let onProgress = index2callback[data.index]
+eventEmitter.addListener("progress", function (data) {
+  let onProgress = index2callback[data.index];
   if (onProgress) {
-    onProgress(data.progress)
+    onProgress(data.progress);
   }
-})
+});
 
 export const ZONE = {
   HUADONG: RNTQiniu.ZONE_HUADONG,
   HUABEI: RNTQiniu.ZONE_HUABEI,
   HUANAN: RNTQiniu.ZONE_HUANAN,
   BEIMEI: RNTQiniu.ZONE_BEIMEI,
-}
+};
 
 export const CODE = {
   UPLOAD_FAILURE: RNTQiniu.ERROR_CODE_UPLOAD_FAILURE,
-}
+};
 
 export function upload(options, onProgress) {
   if (onProgress) {
-    options.index = ++count
-    index2callback[options.index] = onProgress
+    options.index = ++count;
+    index2callback[options.index] = onProgress;
   }
-  return RNTQiniu.upload(options)
-    .finally(() => {
+  const res = RNTQiniu.upload(options);
+  if (res.finally) {
+    res.finally(() => {
       if (onProgress) {
-        delete index2callback[options.index]
+        delete index2callback[options.index];
       }
-    })
+    });
+  }
+  return res;
 }
